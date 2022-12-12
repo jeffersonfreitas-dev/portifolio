efc353f161cbd273f48eb924294251e8de9e198d
jeffersonfreitas-dev <jefferson.dev21@gmail.com>
2022-12-12 20:53:08 -0300

    Itens de menu

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,14 +1,29 @@
 <template>
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 h-16 w-full bg-jeff-green-1">
-      <div
-        class="flex h-full flex-nowrap border-b border-solid bg-jeff-green-1 px-8"
-      >
-        <a
-          href="#"
-          class="text-bg-white text-1xl flex h-full items-center text-xl text-white"
-          >{{ company }}</a
-        >
+      <div class="flex h-full flex-nowrap px-8">
+        <div class="ml-12 flex w-full">
+          <a
+            v-bind:href="url"
+            class="text-bg-white text-1xl flex h-full items-center text-xl text-white"
+            >{{ company }}</a
+          >
+        </div>
+        <div class="mr-12 flex w-full justify-end">
+          <nav class="h-full">
+            <ul class="flex h-full list-none text-white">
+              <li
+                v-for="menuItem in menuItens"
+                v-bind:key="menuItem"
+                class="ml-9 h-full"
+              >
+                <a href="#" class="flex h-full items-center py-2.5">{{
+                  menuItem
+                }}</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   </header>
@@ -20,6 +35,8 @@ export default {
   data() {
     return {
       company: "Jefferson Freitas",
+      url: "https://jeffersonfreitas-dev.github.io",
+      menuItens: ["Sobre", "Projetos", "Contato", "Blog"],
     };
   },
 };
